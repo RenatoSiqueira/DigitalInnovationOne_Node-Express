9c9319ffaa249da43fd74c2e1135141af40e49a8
Renato Siqueira <renatoelysiqueira@gmail.com>
2019-09-15 00:13:42 -0300

    Seção I

--- Seção I/routes/userRoute.js
+++ Seção I/routes/userRoute.js
@@ -42,6 +42,11 @@ const userRoute = (app) => {
             }))
             res.status(200).send('OK')
         })
+        .delete((req, res) => {
+            const users = getUsers()
+            saveUser(users.filter(user => user.id !== req.params.id))
+            res.status(200).send('OK')
+        })
 }
 
 module.exports = userRoute
